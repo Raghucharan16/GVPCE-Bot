cb7bfe53c202b288728bf6a9e9729cc2dfd73f18
Narra_Venkata_Raghu_Charan <narravenkataraghucharan03@gmail.com>
2023-12-25 22:34:01 +0530

    Update handlers.py

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,1 +1,33 @@
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from database import get_papers_by_department_and_year, save_paper
+from keyboards import get_main_keyboard, get_departments_keyboard, get_years_keyboard
+
+# Define conversation states
+CHOOSING_DEPARTMENT, CHOOSING_YEAR, UPLOADING_PAPER, SEARCHING_PAPERS = range(4)
+
+# Define handlers for each state
+def start(update, context):
+    # ... (same as before)
+
+def handle_message(update, context):
+    # ... (same as before)
+
+def handle_choosing_department(update, context):
+    # ... Handle department selection
+
+def handle_choosing_year(update, context):
+    # ... Handle year selection
+
+def handle_uploading_paper(update, context):
+    # ... Handle paper upload
+
+def handle_searching_papers(update, context):
+    # ... Handle paper search
+
+# ... Add more handlers as needed
+
+# Register handlers with ConversationHandler
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(ConversationHandler())
 
