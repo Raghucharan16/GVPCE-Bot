e6532c3bd60794021d681a09867b7c877db9005c
Narra_Venkata_Raghu_Charan <narravenkataraghucharan03@gmail.com>
2023-12-31 22:03:45 +0530

    Delete database directory

--- database/database.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-# ... Connect to database
-
-def get_papers_by_department_and_year(department, year):
-    # ... Implement query logic using your chosen database
-
-def save_paper(paper_data):
-    # ... Implement insert logic using your chosen database
