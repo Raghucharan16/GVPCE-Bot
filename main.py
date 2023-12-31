cb0f74f0a6c3999a3e8752059bf74c3a09ebe4c9
Narra_Venkata_Raghu_Charan <narravenkataraghucharan03@gmail.com>
2023-12-31 21:56:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
-import telegram
-from bot import dispatcher
-from config import TOKEN
+import telepot
+from telepot.loop import MessageLoop
+from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
+import pymongo
+import urllib.parse
+import time
 
-# Create the bot instance
-bot = telegram.Bot(token=TOKEN)
+# Placeholders - Replace with your actual values
+YOUR_DATABASE_URL = "mongodb://your_username:your_password@your_database_host:your_database_port/your_database_name"
+YOUR_BOT_TOKEN = "123456789:YourActualBotToken"
 
-# Start the bot
-dispatcher.start(bot)
+# Escape username and password for MongoDB connection
+username = urllib.parse.quote_plus("your_username")
+password = urllib.parse.quote_plus("your_password")
+uri = "mongodb://" + username + ":" + password + "@" + YOUR_DATABASE_URL[10:]
 
-# Keep the bot running
-dispatcher.idle()
+# Connect to MongoDB
+client = pymongo.MongoClient(uri)
+db = client['question_papers']
+papers_collection = db['papers']
+
+# Telegram bot setup
+bot = telepot.Bot(YOUR_BOT_TOKEN)
+
+def handle_choosing_subject(update, context):
+    departments = ['CSE', 'ECE', 'Mechanical', 'IT', 'Civil', 'Chemical', 'EEE']  # Available departments
+
+    department_buttons = []
+    for department in departments:
+        department_button = InlineKeyboardButton(department, callback_data=department)
+        department_buttons.append([department_button])
+
+    reply_markup = InlineKeyboardMarkup(department_buttons)
+    update.message.reply_text("Choose your department:", reply_markup=reply_markup)
+
+def handle_chosen_department(update, context):
+    chosen_department = update.callback_query.data
+    context.user_data['department'] = chosen_department
+
+    # ... (Rest of your code to get papers based on department and continue the flow)
+
+def send_paper(update, context):
+    # ... (Code to send the paper as before)
+
+# Start the bot and handle messages
+MessageLoop(bot, {'chat': handle_choosing_subject,
+                   'callback_query': handle_chosen_department,  # Handle department choice
+                   'callback_query': send_paper}).run_as_thread()
+
+print('Bot is listening ...')
+
+# Keep the program running (add other functionalities here as needed)
+while 1:
+    time.sleep(10)
